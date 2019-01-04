cdd7f62bc05a90ca781f1c40b6ac31ccbcff2697
roushanj <roushan.jha0709@gmail.com>
2019-01-04 15:54:47 +0530

    server added

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	C "./controller"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	gin.SetMode(gin.DebugMode)
+
+	r := gin.New()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	r.POST("/cabrequest", C.RequestRide)
+
+	r.Run(":8080")
 
 }
