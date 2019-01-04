989431203465072a2be0a24dd29cfb1a2e42e3e1
roushanj <roushan.jha0709@gmail.com>
2019-01-04 15:57:23 +0530

    db added

---
+++ dbPool/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var err error
+var db *sql.DB
+
+func DB() *sql.DB {
+
+	conn := "user:password@tcp(url:3306)/dbname"
+
+	db, err = sql.Open("mysql", conn)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return db
+
+}
