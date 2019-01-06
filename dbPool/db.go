7bae1d7cd4805a41b3d9b2a16472c784cf702798
Roushan Jha <roushan@Roushans-MacBook-Air.local>
2019-01-06 14:33:41 +0530

    db added

--- dbPool/db.go
+++ dbPool/db.go
@@ -12,7 +12,7 @@ var db *sql.DB
 
 func DB() *sql.DB {
 
-	conn := "user:password@tcp(url:3306)/dbname"
+	conn := "user:password@tcp(dburl:3306)/dbname"
 
 	db, err = sql.Open("mysql", conn)
 	if err != nil {
